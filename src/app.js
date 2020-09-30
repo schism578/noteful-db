33838b79837bf16ac254803427af9acd3b568af7
Adrian Marquis <adrian.marquis78@gmail.com>
2020-09-30 09:58:42 -0400

    early stage

--- src/app.js
+++ src/app.js
@@ -7,27 +7,22 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+}))
 
-app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
-
 app.use(function errorHandler(error, req, res, next) {
-        let response
-        if (NODE_ENV === 'production') {
-            response = { error: { message: 'server error' } }
-        } else {
-            console.error(error)
-            response = { message: error.message, error }
-        }
-        res.status(500).json(response)
-    })
+    let response
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+})
 
 module.exports = app
