13f1a3e1ac462f423c864ebb8871b80efcc364a1
Adrian Marquis <adrian.marquis78@gmail.com>
2020-10-07 11:23:05 -0400

    error handler logging

--- src/app.js
+++ src/app.js
@@ -36,6 +36,7 @@ app.use(function validateBearerToken(req, res, next) {
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
+        console.error(error)
         response = { error: { message: 'server error' } }
     } else {
         console.error(error)
